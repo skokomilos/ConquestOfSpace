861ced78ecc63e95aacac743078af19e6b5a5d5e
skokomilos <berkaskoko@gmail.com>
2022-03-24 12:06:16 +0100

    add remote-dto for launches

---
+++ app/src/main/java/com/space/conquestofspace/data/remote/dto/LocationDTO.kt
@@ -0,0 +1,9 @@
+package com.space.conquestofspace.data.remote.dto
+
+data class LocationDTO(
+    val id: Int,
+    val url: String,
+    val name: String,
+    val country_code: String,
+    val map_image: String
+)
---
+++ app/src/main/java/com/space/conquestofspace/data/remote/dto/StatusDTO.kt
@@ -0,0 +1,8 @@
+package com.space.conquestofspace.data.remote.dto
+
+data class StatusDTO(
+    val id: Int,
+    val name: String,
+    val abbrev: String,
+    val description: String
+)
