9bf1f9ce191447753210f590377333e39654fb20
skokomilos <berkaskoko@gmail.com>
2022-03-25 16:32:44 +0100

    add GsonParser.kt, Converters.kt

--- app/src/main/java/com/space/conquestofspace/data/local/SpaceDB.kt
+++ app/src/main/java/com/space/conquestofspace/data/local/SpaceDB.kt
@@ -1,6 +1,7 @@
 package com.space.conquestofspace.data.local
 
 import androidx.room.Database
+import androidx.room.TypeConverters
 import com.space.conquestofspace.data.local.dao.LaunchDao
 import com.space.conquestofspace.data.local.entity.LaunchEntity
 
@@ -8,6 +9,7 @@ import com.space.conquestofspace.data.local.entity.LaunchEntity
     entities = [LaunchEntity::class],
     version = 1
 )
+@TypeConverters(Converters::class)
 abstract class SpaceDB {
 
     abstract val dao: LaunchDao
