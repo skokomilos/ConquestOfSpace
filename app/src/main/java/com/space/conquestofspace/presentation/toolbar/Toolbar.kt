87cd262f41d39fa9760a60921d0b6a92208279c2
parsable-mskoko <milos.skoko@parsable.com>
2023-03-28 16:04:38 +0200

    comment Images in CollapsingToolbar and use Text instead of them for now, Change position (x,y) of those Text fields in CollapsingToolbarLayout

--- app/src/main/java/com/space/conquestofspace/presentation/toolbar/Toolbar.kt
+++ app/src/main/java/com/space/conquestofspace/presentation/toolbar/Toolbar.kt
@@ -1,20 +1,26 @@
 package com.space.conquestofspace.presentation.toolbar
 
+import android.util.Log
 import androidx.annotation.DrawableRes
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.layout.Layout
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.compose.ui.util.lerp
 import com.space.conquestofspace.R
 import com.space.conquestofspace.presentation.ui.theme.ConquestOfSpaceAppTheme
@@ -78,34 +84,74 @@ fun CollapsingToolbar(
                 .fillMaxSize()
         ) {
             CollapsingToolbarLayout(progress = progress) {
-                Image(
-                    painter = painterResource(id = R.drawable.baseline_rocket_launch_24),
-                    contentDescription = null,
+                Text(
+                    text = "International",
+                    style = TextStyle(
+                        fontSize = 30.sp,
+                        fontWeight = FontWeight.Bold,
+                        color = Color.White,
+                        textAlign = TextAlign.Center
+                    ),
                     modifier = Modifier
                         .padding(logoPadding)
                         .height(32.dp)
                         .wrapContentWidth()
-                        .graphicsLayer { alpha = ((0.25f - progress) * 4).coerceIn(0f, 1f) },
-                    colorFilter = ColorFilter.tint(MaterialTheme.colors.onPrimary)
+                    // .graphicsLayer { alpha = ((0.25f - progress) * 4).coerceIn(0f, 1f) }
                 )
-                Image(
-                    painter = painterResource(id = R.drawable.baseline_cloud_24),
-                    contentDescription = null,
+                Text(
+                    text = "Space",
+                    style = TextStyle(
+                        fontSize = 30.sp,
+                        fontWeight = FontWeight.Bold,
+                        color = Color.White,
+                        textAlign = TextAlign.Center
+                    ),
                     modifier = Modifier
                         .padding(logoPadding)
-                        .height(headlineOneHeight)
-                        .wrapContentWidth(),
-                    colorFilter = ColorFilter.tint(MaterialTheme.colors.onPrimary)
+                        .height(32.dp)
+                        .wrapContentWidth()
                 )
-                Image(
-                    painter = painterResource(id = R.drawable.baseline_star_24),
-                    contentDescription = null,
+                Text(
+                    text = "Station",
+                    style = TextStyle(
+                        fontSize = 30.sp,
+                        fontWeight = FontWeight.Bold,
+                        color = Color.White,
+                        textAlign = TextAlign.Center
+                    ),
                     modifier = Modifier
                         .padding(logoPadding)
-                        .height(headlineOneHeight)
-                        .wrapContentWidth(),
-                    colorFilter = ColorFilter.tint(MaterialTheme.colors.onPrimary)
+                        .height(32.dp)
+                        .wrapContentWidth()
                 )
+//                Image(
+//                    painter = painterResource(id = R.drawable.baseline_rocket_launch_24),
+//                    contentDescription = null,
+//                    modifier = Modifier
+//                        .padding(logoPadding)
+//                        .height(32.dp)
+//                        .wrapContentWidth()
+//                        .graphicsLayer { alpha = ((0.25f - progress) * 4).coerceIn(0f, 1f) },
+//                    colorFilter = ColorFilter.tint(MaterialTheme.colors.onPrimary)
+//                )
+//                Image(
+//                    painter = painterResource(id = R.drawable.baseline_cloud_24),
+//                    contentDescription = null,
+//                    modifier = Modifier
+//                        .padding(logoPadding)
+//                        .height(headlineOneHeight)
+//                        .wrapContentWidth(),
+//                    colorFilter = ColorFilter.tint(MaterialTheme.colors.onPrimary)
+//                )
+//                Image(
+//                    painter = painterResource(id = R.drawable.baseline_star_24),
+//                    contentDescription = null,
+//                    modifier = Modifier
+//                        .padding(logoPadding)
+//                        .height(headlineOneHeight)
+//                        .wrapContentWidth(),
+//                    colorFilter = ColorFilter.tint(MaterialTheme.colors.onPrimary)
+//                )
             }
         }
     }
@@ -132,36 +178,48 @@ private fun CollapsingToolbarLayout(
         ) {
             val expandedHorizontalGuideline = (constraints.maxHeight * 0.4f).roundToInt()
             val collapsedHorizontalGuideline = (constraints.maxHeight * 0.5f).roundToInt()
-
-            val logo = placeables[0]
-            val international = placeables[1]
-            val spaceStation = placeables[2]
-
-            logo.placeRelative(
-                x = 0,
-                y = collapsedHorizontalGuideline - logo.height / 2
+            Log.d(
+                "TAG",
+                "CollapsingToolbarLayout: $expandedHorizontalGuideline and $collapsedHorizontalGuideline"
             )
+
+            val international = placeables[0]
+            val space = placeables[1]
+            val station = placeables[2]
+
             international.placeRelative(
                 x = lerp(
-                    start = logo.width,
+                    start = 0,
                     stop = constraints.maxWidth / 2 - international.width,
                     fraction = progress
                 ),
                 y = lerp(
                     start = collapsedHorizontalGuideline - international.height / 2,
-                    stop = expandedHorizontalGuideline - international.height,
+                    stop = expandedHorizontalGuideline - station.height,
                     fraction = progress
                 )
             )
-            spaceStation.placeRelative(
+            space.placeRelative(
                 x = lerp(
-                    start = logo.width + international.width,
-                    stop = constraints.maxWidth / 2 - spaceStation.width,
+                    start = international.width,
+                    stop = constraints.maxWidth / 2 - international.width,
                     fraction = progress
                 ),
                 y = lerp(
-                    start = collapsedHorizontalGuideline - spaceStation.height / 2,
+                    start = collapsedHorizontalGuideline - international.height / 2,
                     stop = expandedHorizontalGuideline,
+                    fraction = progress
+                )
+            )
+            station.placeRelative(
+                x = lerp(
+                    start = international.width + space.width,
+                    stop = constraints.maxWidth / 2 - international.width,
+                    fraction = progress
+                ),
+                y = lerp(
+                    start = collapsedHorizontalGuideline - station.height / 2,
+                    stop = expandedHorizontalGuideline + international.height,
                     fraction = progress
                 )
             )
