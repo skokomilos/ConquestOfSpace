fe354fb8b0ed4ce1fba3cde3fe6c13b6adf55e09
skokomilos <berkaskoko@gmail.com>
2023-05-09 17:42:54 +0200

    add thumbnail image to card change position of textView(name) in card.

--- app/src/main/java/com/space/conquestofspace/presentation/iss/AstronautDetailScreen.kt
+++ app/src/main/java/com/space/conquestofspace/presentation/iss/AstronautDetailScreen.kt
@@ -2,28 +2,36 @@ package com.space.conquestofspace.presentation.iss
 
 import androidx.compose.foundation.ScrollState
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Card
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.constraintlayout.compose.ConstraintLayout
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.space.conquestofspace.R
 import com.space.conquestofspace.data.remote.dto.astronaut.Agency
 import com.space.conquestofspace.data.remote.dto.astronaut.AstronautResponse
 import com.space.conquestofspace.data.remote.dto.astronaut.StatusXXX
@@ -96,16 +104,18 @@ fun AstronautDetailsContent(
                         start.linkTo(parent.start)
                         end.linkTo(parent.end)
                     },
-                name = astronaut.name
+                name = astronaut.name,
+                imageUrl = astronaut.profile_image_thumbnail
             )
         }
     }
 }
 
 @Composable
-fun AstronautsData(modifier: Modifier = Modifier, name: String) {
+fun AstronautsData(modifier: Modifier = Modifier, name: String, imageUrl: String) {
     Card(
-        modifier = modifier,
+        modifier =
+        modifier.padding(8.dp),
         shape = RoundedCornerShape(16.dp),
         elevation = 8.dp
     ) {
@@ -114,13 +124,28 @@ fun AstronautsData(modifier: Modifier = Modifier, name: String) {
                 .background(Color.Blue)
                 .fillMaxWidth()
                 .height(200.dp)
+                .padding(8.dp),
         ) {
-            Text(
-                modifier = Modifier.fillMaxWidth(),
-                text = name,
-                textAlign = TextAlign.Center,
-                fontSize = 20.sp
-            )
+            Column(
+                // horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center
+            ) {
+                com.skydoves.landscapist.glide.GlideImage(
+                    imageModel = { imageUrl },
+                    modifier = Modifier
+                        .size(88.dp)
+                        .clip(CircleShape),
+                    previewPlaceholder = R.drawable.astronaut
+                )
+                Text(
+                    modifier = Modifier.fillMaxWidth(),
+                    text = name,
+                    textAlign = TextAlign.Start,
+                    fontWeight = FontWeight.Bold,
+                    style = MaterialTheme.typography.h4,
+                    fontSize = 20.sp
+                )
+            }
         }
         // Content of the card goes here
     }
