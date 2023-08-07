b1ade929272093f4349eb807da48d6017af5692a
skokomilos <berkaskoko@gmail.com>
2023-08-07 20:24:37 +0200

    add elements for Image

--- app/src/main/java/com/space/conquestofspace/presentation/agencies/detail/AgencyDetailScreen.kt
+++ app/src/main/java/com/space/conquestofspace/presentation/agencies/detail/AgencyDetailScreen.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
@@ -18,17 +19,28 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.Layout
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.lerp
+import androidx.compose.ui.util.lerp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.space.conquestofspace.presentation.iss.SpaceImage
 import com.space.conquestofspace.presentation.ui.theme.ConquestOfSpaceAppTheme
-
+import kotlin.math.max
+import kotlin.math.min
 /**
  *
  * @author berka on 7/24/23
  */
 private val MinTitleOffset = 56.dp
+private val MinImageOffset = 12.dp
 private val GradientScroll = 180.dp
+private val ImageOverlap = 115.dp
+private val ExpandableImageSize = 300.dp
+private val MaxTitleOffset = ImageOverlap + MinTitleOffset + GradientScroll
+private val CollapsedImageSize = 150.dp
+private val HzPadding = Modifier.padding(horizontal = 24.dp)
 
 @Composable
 fun AgencyDetailScreen(
@@ -49,7 +61,7 @@ fun AgencyDetailContent(state: AgencyDetailState) {
         Header()
         Body(scroll)
         Title(scroll.value, "Neki naslov")
-        Image(state)
+        Image(state.agency?.image_url) { scroll.value }
     }
 }
 
@@ -98,8 +110,47 @@ private fun Body(scroll: ScrollState) {
 }
 
 @Composable
-fun Image(state: AgencyDetailState) {
-    SpaceImage(model = state.agency?.image_url, contentDescription = "", imageHeight = 120.dp)
+fun Image(imageUrl: String?, scrollProvider: () -> Int) {
+    val collapseRange = with(LocalDensity.current) { (MaxTitleOffset - MinTitleOffset).toPx() }
+    val collapseFractionProvider = {
+        (scrollProvider() / collapseRange).coerceIn(0f, 1f)
+    }
+
+    CollapsingImageLayout(
+        collapseFractionProvider,
+        HzPadding.then(Modifier.statusBarsPadding())
+    ) {
+        SpaceImage(
+            model = imageUrl,
+            contentDescription = "",
+            imageHeight = 120.dp
+        )
+    }
+}
+
+@Composable
+fun CollapsingImageLayout(
+    collapseFractionProvider: () -> Float,
+    modifier: Modifier = Modifier,
+    content: @Composable () -> Unit
+) {
+    Layout(
+        modifier = modifier,
+        content = content
+    ) { measurables, constraints ->
+        check(measurables.size == 1)
+
+        val collapseFraction = collapseFractionProvider()
+        val imageMaxSize = min(ExpandableImageSize.roundToPx(), constraints.maxWidth)
+        val imageMinSize = max(CollapsedImageSize.roundToPx(), constraints.minWidth)
+        val imageWidth = lerp(imageMaxSize, imageMinSize, collapseFraction)
+        val imageY = lerp(MinTitleOffset, MinImageOffset, collapseFraction).roundToPx()
+        layout(
+            width = constraints.minWidth,
+            height = imageY + imageWidth
+        ) {
+        }
+    }
 }
 
 @Composable
